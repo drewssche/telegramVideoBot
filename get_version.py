72b7b2df480f83b967bc6cb028d3287c7160aa78
Drews <schegolev.andrey.sergeevich@gmail.com>
2025-03-27 23:59:45 +0300

    2045

--- get_version.py
+++ get_version.py
@@ -6,8 +6,12 @@ with open('version.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 version = data['version']
+# Объединяем changelog с переносами строк \n и префиксом "- "
 changelog = '- ' + '\n- '.join(data['changelog'])
 
+# Для записи в $GITHUB_OUTPUT используем многострочный синтаксис
 with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as f:
     f.write(f'VERSION={version}\n')
-    f.write(f'CHANGELOG={changelog}\n')
+    f.write('CHANGELOG<<EOF\n')
+    f.write(changelog + '\n')
+    f.write('EOF\n')
